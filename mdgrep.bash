2af2c72ab3489af5cd114ed4fc37ca819ff036a0
SPREEKDOS <72156842+SPREEKDOS@users.noreply.github.com>
2025-01-25 23:38:41 +0300

    Update mdgrep.bash

--- mdgrep.bash
+++ mdgrep.bash
@@ -11,30 +11,65 @@
 #       1 if no pattern matched
 #       2 if an error occured
 #######################
-set -x
-ELEMENT_REGEXP="${1:?}"
-REGEXP="${2-''}"
-FILE="$(printf '%s\n' "${3:?}" | envsubst)"
-VALID_MD_ELEMENTS=('#' '##' '###')
-# headings
-if [[ ! -f "$FILE" ]] ; then printf '%s\n' "${SCRIPT_NAME} | file doesn't exist" ; exit 1 ; fi
+#TODO line 34: warning: command substitution: ignored null byte in input
 
+# shell options
+set -o xtrace
+
+# variable
+USAGE=
+ELEMENT_REGEXP="${1:?}"
+FILE="$( envsubst <<< "${2:?}" )"
+VALID_MD_ELEMENTS=('#' '##' '###')
+
+# function 
+usage() { 
+    printf '%s\n' "usage: ${SCRIPT_NAME} ELEMENT_REGEXP FILE \
+    options: 
+-r REGEXP" 
+    exit 0 
+}
+
+while getopts "r:h" OPTS
+do
+    case ${OPTS} in
+        r)
+            REGEXP="${OPTARG:?}"
+            ;;
+        *|h)
+            usage
+            ;;
+    esac
+done
+#shift $(($OPTIND - 1))
+
+# logic
+if [[ "$#" -eq 0  ]] ; then usage ; fi
+if [[ ! -f "$FILE" ]] ; then printf '%s\n' "${SCRIPT_NAME} | file doesn't exist" ; exit 1 ; fi
 for element in "${VALID_MD_ELEMENTS[@]}" ; do
-    if [[ "$ELEMENT_REGEXP" =~ ^"$element"\w* ]] ; then printf '%s\n' "${SCRIPT_NAME} | ELEMENT_REGEXP is not a valid element" ; exit 1 ; fi
+    if [[ "$ELEMENT_REGEXP" =~ ^"$element"[[:alnum:]]* ]] ; then 
+        break
+    else 
+        printf '%s\n' "${SCRIPT_NAME} | ELEMENT_REGEXP is not a valid element"
+        exit 1 
+    fi
 done
 
 case "$ELEMENT_REGEXP" in 
     *[[:alnum:]]*) 
-        result="$(grep -Poz "(?ms)^${ELEMENT_REGEXP}\s?.*?(?=^${ELEMENT_REGEXP%%[[:alnum:]]*}\s?[^${ELEMENT_REGEXP%%[[:alnum:]]*}]+|\Z)" "$FILE" \
-        | tr '\0' '\n' \
-        | sed "1d" )"
-#        result="${result//$'\0'/$'\n'}"
-#        result="${result##*$'\n'}"
+        result="$(grep --perl-regexp \
+        --only-matching \
+        --null-data \
+        "(?ms)^${ELEMENT_REGEXP}\s?.*?(?=^${ELEMENT_REGEXP%%[[:alnum:]]*}\s?[^${ELEMENT_REGEXP%%[[:alnum:]]*}]+|\Z)" "$FILE" )"
+        result="${result//$'\0'/$'\n'}"
+        result="${result#*$'\n'}"
 
         result="${result:?"the specified ${ELEMENT_REGEXP} header is not found in ${FILE}"}"
     ;;
     *)
-        result="$(grep -Po "^${ELEMENT_REGEXP%%[[:alnum:]]*}\s?[^${ELEMENT_REGEXP%%[[:alnum:]]*}]+$" "$FILE")"
+        result="$(grep --perl-regexp \
+        --only-matching \
+        "^${ELEMENT_REGEXP%%[[:alnum:]]*}\s?[^${ELEMENT_REGEXP%%[[:alnum:]]*}]+$" "$FILE")"
         result="${result:?"${ELEMENT_REGEXP} headers are not found in ${FILE}"}"
     ;;
 esac
